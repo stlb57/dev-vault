f509793b411ec3724876b7cf5b358211333ad168
lb57 <saarthaktyagi1@gmail.com>
2026-06-15 23:30:38 +0530

    updated dev-vault-cli/models/file_store.go

--- dev-vault-cli/store/file_store.go
+++ dev-vault-cli/store/file_store.go
@@ -43,3 +43,49 @@ func (f FileStore) save(snippets []models.Snippet) error {
 
 	return nil
 }
+
+func (f FileStore) GetAll() ([]models.Snippet, error) {
+	snippets, err := f.load()
+	if err != nil {
+		return nil, err
+	}
+	return snippets, nil
+}
+
+func (f FileStore) GetByID(id int) (models.Snippet, error) {
+	snippets, err := f.load()
+	if err != nil {
+		return models.Snippet{}, err
+	}
+	for i := 0; i < len(snippets); i++ {
+		if snippets[i].Id == id {
+			return snippets[i], nil
+		}
+	}
+	return models.Snippet{}, ErrNotFound
+}
+
+func (f FileStore) Delete(id int) error {
+	snippets, err := f.load()
+	if err != nil {
+		return err
+	}
+	found := false
+	var updated []models.Snippet
+
+	for _, snippet := range snippets {
+		if snippet.Id == id {
+			found = true
+			continue
+		}
+		updated = append(updated, snippet)
+	}
+
+	if !found {
+		return ErrNotFound
+	}
+
+	return f.save(updated)
+}
+
+func (f FileStore) Search()
